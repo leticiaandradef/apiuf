2af32033e23b5ace0090257941a2be0c611e0666
leticiaandradef <lelectpm2021@gmail.com>
2024-11-28 17:06:01 -0400

    apiuf

--- servicos/servico.js
+++ servicos/servico.js
@@ -1,14 +1,14 @@
-import colecaoUf from "../dados/dados";
+import  colecaoUf  from '../dados/dados.js';
 
 export const buscarUfs = () => {
-    return colecaoUf;
+  return colecaoUf;
 }
 
 export const buscarUfsPorNome = (nomeUf) => {
-    return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()));
+  return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()));
 };
 
-export const buscarUfporId = (id) => {
-    const idUF = parseInt(id);
-    return colecaoUf.find(uf => uf.id === idUF);
+export const buscarUfPorId = (id) => {
+  const idUF = parseInt(id);
+  return colecaoUf.find(uf => uf.id === idUF);
 }
